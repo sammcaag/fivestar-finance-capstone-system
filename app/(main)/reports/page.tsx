aecb948d823e87e5c5aeb81de249130598bcb719
Samm Caagbay <sammcaag@gmail.com>
2025-10-21 01:05:23 +0800

    fix: fixing vercel build eslint errors

--- app/(main)/reports/page.tsx
+++ app/(main)/reports/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useState } from "react";
 import { ContentLayout } from "@/components/staff-panel/content-layout";
 import BreadcrumbPages from "@/components/BreadcrumbPages";
 import MainHeader from "@/components/MainHeader";
