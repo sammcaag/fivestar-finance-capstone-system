83e287a15ccf9e02d7f8a2d8f09dc3bce17dc5f5
Rey Daug <daug.rey34@gmail.com>
2025-11-06 14:38:19 +0800

    chore: removing unnecessary imports and apply dry principles

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,18 +1,18 @@
 "use client";
-import { motion } from "framer-motion";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import { ContentLayout } from "@/components/staff-panel/content-layout";
 import BreadcrumbPages from "@/components/BreadcrumbPages";
 import { dashboardStatistics } from "@/features/clients/data/client-mock-stats";
 import MainHeader from "@/components/MainHeader";
-import {
-  dashboardQuickActions,
-  dashboardMotionContainer,
-  dashboardMotionItem,
-} from "@/lib/dashboard-vars";
 import StatisticsCard from "@/components/StatisticsCard";
 import TabListCustomComp from "@/components/TabListCustomComp";
-import { MapPin } from "lucide-react";
+import {
+  Calculator,
+  FileChartColumn,
+  MapPin,
+  Search,
+  UserPlus,
+} from "lucide-react";
 import MainDashboardTable from "@/features/clients/components/tables/MainDashboardTable";
 import { useEffect } from "react";
 import { clientTableData } from "@/features/clients/data/client-mock";
@@ -23,6 +23,29 @@ import { appointmentsColumnDefinition } from "@/features/loans/appointments/comp
 const dashboardTabs = [
   { value: "overview", label: "Overview" },
   { value: "appointments", label: "Mobile Appointments" },
+];
+
+const dashboardQuickActions = [
+  {
+    label: "Register New Client",
+    href: "/clients/register",
+    icon: UserPlus,
+  },
+  {
+    label: "New Client Computation",
+    href: "/loans/computations/new-client",
+    icon: Calculator,
+  },
+  {
+    label: "View Reports",
+    href: "/reports",
+    icon: FileChartColumn,
+  },
+  {
+    label: "Search Client",
+    href: "/#",
+    icon: Search,
+  },
 ];
 
 export default function DashboardPage() {
@@ -46,43 +69,35 @@ export default function DashboardPage() {
         icon={MapPin}
       />
 
-      <motion.div
-        variants={dashboardMotionContainer}
-        initial="hidden"
-        animate="show"
-      >
-        <StatisticsCard statistics={dashboardStatistics} />
+      <StatisticsCard statistics={dashboardStatistics} />
 
-        <motion.div variants={dashboardMotionItem} className="mt-10">
-          <Tabs defaultValue="overview" className="w-full">
-            {/* Tab List */}
-            <TabListCustomComp tabs={dashboardTabs} />
-            {/* Tab Content */}
-            <TabsContent
-              value="overview"
-              className="mt-4 flex flex-col md:flex-row gap-8"
-            >
-              <MainDashboardTable
-                title="Recent Client & Loan Activities"
-                description="A quick overview of your most recent client transactions, loan updates, and payment statuses."
-                href="/clients"
-                data={clientTableData}
-                columns={clientsColumnDefinition(true)}
-              />
-            </TabsContent>
+      <Tabs defaultValue="overview" className="w-full">
+        {/* Tab List */}
+        <TabListCustomComp tabs={dashboardTabs} />
+        {/* Tab Content */}
+        <TabsContent
+          value="overview"
+          className="mt-4 flex flex-col md:flex-row gap-8"
+        >
+          <MainDashboardTable
+            title="Recent Client & Loan Activities"
+            description="A quick overview of your most recent client transactions, loan updates, and payment statuses."
+            href="/clients"
+            data={clientTableData}
+            columns={clientsColumnDefinition(true)}
+          />
+        </TabsContent>
 
-            <TabsContent value="appointments" className="mt-4">
-              <MainDashboardTable
-                title="Recent Mobile Appointments"
-                description="See the latest scheduled and completed client appointments. Visit the Appointments tab for the full schedule."
-                href="loans/appointments"
-                data={appointmentsData}
-                columns={appointmentsColumnDefinition(true)}
-              />
-            </TabsContent>
-          </Tabs>
-        </motion.div>
-      </motion.div>
+        <TabsContent value="appointments" className="mt-4">
+          <MainDashboardTable
+            title="Recent Mobile Appointments"
+            description="See the latest scheduled and completed client appointments. Visit the Appointments tab for the full schedule."
+            href="loans/appointments"
+            data={appointmentsData}
+            columns={appointmentsColumnDefinition(true)}
+          />
+        </TabsContent>
+      </Tabs>
     </ContentLayout>
   );
 }
